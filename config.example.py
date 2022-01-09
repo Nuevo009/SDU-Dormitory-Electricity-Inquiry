ee31282c7a9d59c624855890dd0da21cfb47315f
Nuevo009 <kianakaslanna6@gmail.com>
2022-01-09 15:25:08 +0800

    no need for WeChat openId

--- config.example.py
+++ config.example.py
@@ -8,8 +8,7 @@ QMSG_API_KEY = ''
 QMSG_USER_ID = ''
 headers = {
 'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 5 Build/RD2A.211001.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.120 Mobile Safari/537.36 MMWEBID/977 MicroMessenger/8.0.16.2040(0x28001037) Process/toolsmp WeChat/arm64 Weixin NetType/WIFI Language/en ABI/arm64',
-'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-'Cookie': 'openId=xxxx; JSESSIONID=xxx'
+'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 }
-data='openId=xxxx&userXq=xxxx&userFj=xxxx'
+data='userXq=xxxx&userFj=xxxx'
 url = 'http://www.stuzf.sdu.edu.cn/wxapp/api/pay/queryElectricity'
